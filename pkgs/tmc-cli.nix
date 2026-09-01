0ee4339d43a887e4771a713a9353772bc25beb74
konradszl <konradszl@pm.me>
2026-09-01 12:36:14 +0200

    feat(pkgs): package tmc-cli for the Java MOOC
    Wrap the upstream release jar with jdk21 and maven on PATH so 'tmc'
    can download, test and submit course exercises.

---
+++ pkgs/tmc-cli.nix
@@ -0,0 +1,42 @@
+{ lib
+, stdenvNoCC
+, fetchurl
+, makeWrapper
+, jdk21
+, maven
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "tmc-cli";
+  version = "0.9.3";
+
+  src = fetchurl {
+    url = "https://github.com/testmycode/tmc-cli/releases/download/${finalAttrs.version}/tmc-cli-${finalAttrs.version}.jar";
+    hash = "sha256-Cx1KAdNgNI24miCPw6Z1NcvkrIXxdHB4LJHMt88E3b4=";
+  };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/tmc-cli
+    cp $src $out/share/tmc-cli/tmc-cli.jar
+
+    makeWrapper ${jdk21}/bin/java $out/bin/tmc \
+      --add-flags "-jar $out/share/tmc-cli/tmc-cli.jar --no-update" \
+      --prefix PATH : ${lib.makeBinPath [ jdk21 maven ]}
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Command-line client for the TestMyCode (TMC) service used by mooc.fi courses";
+    homepage = "https://github.com/testmycode/tmc-cli";
+    license = lib.licenses.mit;
+    mainProgram = "tmc";
+    platforms = lib.platforms.unix;
+  };
+})
